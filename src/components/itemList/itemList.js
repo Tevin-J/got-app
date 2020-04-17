a6bcbcb8675853703e50f93edae8a45f0ebd1da7
Anton <anton.revta@gmail.com>
2020-04-17 11:41:11 +0300

    make hooks in withData

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styled from "styled-components";
 import withData from "./withData";
-import gotAPI from "../../api/api";
 
 const ItemListBlock = styled.ul`
     cursor: pointer;
@@ -12,37 +11,32 @@ const ItemListBlock = styled.ul`
 `
 /*Компоненту ItemList сделали независимой, то что она отрисует зависит от пропсов
 которые в нее придут*/
-class ItemList extends Component {
-
+const ItemList = ({renderItem, onItemSelected, data}) => {
     /*в этой ф-и отрисовываем все айтемы которые придут в стейт после запроса
     на сервер в componentDidMount и при клике на айтем вызываем ф-ю из пропсов с его id*/
-    renderItems = (arr) => {
+    const renderItems = (arr) => {
         return arr.map((item) => {
             const {id} = item
             /*вызываем из пропсов ф-ю renderItem и передаем в нее поочередно каждый айтем и
             получаем те данные которые мы из родителя в результат этой ф-и вернем. таким образом
             мы снаружи управляем тем, что вернет нам эта ф-я*/
-            const label = this.props.renderItem(item)
+            const label = renderItem(item)
             return (
                 <li key={id} className="list-group-item"
-                    onClick={() => this.props.onItemSelected(id)}>
+                    onClick={() => onItemSelected(id)}>
                     {label}
                 </li>
             )
         })
     }
-    render() {
-        /*воспользовались HOC withData и получили массив айтемов из него через пропсы*/
-        const {data} = this.props
-        /*если у нас есть в стейте массив айтемов, то получаем его с помощью вызова ф-и
-        которая отрисует их*/
-        const items = this.renderItems(data)
-        return (
-            <ItemListBlock className="list-group">
-                {items}
-            </ItemListBlock>
-        );
-    }
+    /*если у нас есть в стейте массив айтемов, то получаем его с помощью вызова ф-и
+    которая отрисует их*/
+    const items = renderItems(data)
+    return (
+        <ItemListBlock className="list-group">
+            {items}
+        </ItemListBlock>
+    );
 }
 /*оборачиваем ItemList хоком withData, который делает запрос на сервер за данными и
 отвечает за отрисовку спиннеров и тп*/
--- src/components/itemList/withData.js
+++ src/components/itemList/withData.js
@@ -1,42 +1,39 @@
 /*всю общую логику по запросам на сервер, отрисовкой спинеров вынесли в HOC*/
 import Spinner from "../spinner/spinner";
 import ErrorMessage from "../errorMessage/errorMessage";
-import React from 'react'
-import booksPage from "../pages/booksPage/booksPage";
+import React, {useState, useEffect} from 'react'
 
 const withData = (View) => {
     return (
-        class extends React.Component {
-            state = {
-                data: null,
-                isError: false
-            }
+        (props) => {
+            debugger
+            const [data, updateData] = useState([])
+            const [isError, toggleIsError] = useState(false)
             /*из пропсов получаем тот запрос на сервер, который необходимо сделать родительской
-            компоненте, будто книги или герои*/
-            componentDidMount() {
-                this.props.getData()
+            компоненте, будто книги или герои, вторым аргументом передаем пустой массив,
+            чтоб у нас не зациклился запрос*/
+            useEffect(() => {
+                debugger
+                props.getData()
                     .then(data => {
-                        this.setState({data})
+                        updateData(data)
                     })
-            }
-            componentDidCatch(error, errorInfo) {
-                this.setState({isError: true})
-            }
-            render() {
-                const {data} = this.state
+            },[])
+            /*componentDidCatch(error) {
+                toggleIsError(true)
+            }*/
                 if (!data) {
                     return <Spinner/>
                 }
-                if (this.state.isError) {
+                if (isError) {
                     return <ErrorMessage/>
                 }
                 return (
                     /*передаем в целевую компоненту props которые передала родительская
                     компонента (booksPage например) и добавляем в нее данные, пришедшие
                     с сервера*/
-                    <View {...this.props} data={data}/>
+                    <View {...props} data={data}/>
                 )
-            }
         }
     )
 }
